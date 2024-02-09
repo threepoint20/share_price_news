e1a68d83ac886471b97f55dbb340642a41915125
threepoint20 <111547726+threepoint20@users.noreply.github.com>
2024-02-09 12:08:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,20 @@
-import yfinance as yf
 import streamlit as st
+import numpy as np
+import plotly.figure_factory as ff
 
-st.write("""
-# Simple Stock Price App
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
 
-Shown are the stock closing price and volume of Google!
+# Group data together
+hist_data = [x1, x2, x3]
 
-""")
+group_labels = ['Group 1', 'Group 2', 'Group 3']
 
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
-tickerSymbol = 'GOOGL'
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
 
-st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.Volume)
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
